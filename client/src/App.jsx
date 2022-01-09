d0ceab7445f56eb0ca7f1fa2e60b00676d0fbbf0
Alon J Adelson <contact@alonadelson.com>
2022-01-09 16:52:19 -0500

    frontend commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Footer, Home, NavBar, Services, Transactions, Loader } from './components';
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <NavBar />
+        <Home />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Loader } from './Loader';
+export { default as Footer } from './Footer';
+export { default as Home } from './Home';
+export { default as NavBar } from './Nav';
+export { default as Services } from './Services';
+export { default as Transactions } from './Transactions';
