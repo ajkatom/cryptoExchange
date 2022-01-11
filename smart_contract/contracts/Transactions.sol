6ae11b423c351d91bebd42fbdb5f2b8dc2141289
ajkatom <contact@alonadelson.com>
2022-01-10 23:42:59 -0500

    contract deployed

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+contract Transactions {
+
+  uint256 transactionCounter;
+
+  event Transfer(address from, address receiver,uint amount,string massage, uint256 timestamp, string keyword);
+
+  struct TransferStruct { 
+    address sender;
+    address receiver;
+    uint amount;
+    string massage;
+    uint256 timestamp;
+    string keyword;
+  }
+
+  TransferStruct[] transactions;
+
+  function addToBlockchain(address payable receiver, uint amount, string memory massage, string memory keyword) public {
+    transactionCounter += 1;
+    transactions.push(TransferStruct(msg.sender, receiver, amount, massage, block.timestamp, keyword));
+
+    emit Transfer(msg.sender, receiver, amount, massage, block.timestamp, keyword);
+  }
+  function getAllTransactions() public view returns(TransferStruct[] memory) {
+    return transactions;
+  }
+  function getTransactionCount() public view returns(uint256) {
+    return transactionCounter;
+  }
+
+}
